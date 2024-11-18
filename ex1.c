fec838b1115eba62877b4220582f8371108fdaed
Yaakov Halperin <halperinyy@gmail.com>
2024-11-18 23:01:29 +0200

    More fixes

--- ex1.c
+++ ex1.c
@@ -42,7 +42,7 @@ int main()
   scanf("%d", &i2);
   t2 = 1 << i2;
   z2 = x2 ^ t2;
-  printf("Number with bit %d toggeld: %d\n",i2,z2);
+  printf("Number with bit %d toggled: %d\n",i2,z2);
 
   // Even - Odd
   printf("\nEven - Odd:\n");
@@ -63,12 +63,12 @@ int main()
   Print only 4 bits, in positions: 3,5,7,11 in the result. */
 
   int x4,y4,t4,h3,h5,h7,h11;
-  printf("Please enter the first number\n");
+  printf("Please enter the first number (octal):\n");
   scanf("%o", &x4);
-  printf("Please enter the seconed number\n");
+  printf("Please enter the second number (octal):\n");
   scanf("%o", &y4);
   t4 = x4 + y4;
-  printf("The sum in hexadecimal:%x\n",t4);
+  printf("The sum in hexadecimal: %X\n",t4);
   h3 = t4 >> 3;
   h3 = h3 & 1;
   h5 = t4 >> 5;
@@ -77,7 +77,7 @@ int main()
   h7 = h7 & 1;
   h11 = t4 >> 11;
   h11 = h11 & 1;
-  printf("The 3,5,7,11 bits are:%d%d%d%d\n",h3,h5,h7,h11);
+  printf("The 3,5,7,11 bits are: %d%d%d%d\n",h3,h5,h7,h11);
 
   printf("Bye!\n");
   
